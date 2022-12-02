5b19ee687ef929f524a4903d2c1fdada27e670da
deeganth <118927167+deeganth@users.noreply.github.com>
2022-12-02 23:21:23 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -27,9 +27,9 @@ async def channel_post(client: Client, message: Message):
     link = f"https://telegram.me/{client.username}?start={base64_string}"
 
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(" 🔗 LINK 🔗", url=f'https://telegram.me/{client.username}?start={base64_string}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(" 🔗 LINK 🔗 ", url=f'https://telegram.me/{client.username}?start={base64_string}')]])
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link} \n \n ``{link}``", reply_markup=reply_markup, disable_web_page_preview = True)
+    await reply_text.edit(f"<b>Here is your link</b>\n\n{link} \n \n `` {link} ``", reply_markup=reply_markup, disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
@@ -45,7 +45,8 @@ async def new_post(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://telegram.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(" 🔗 LINK 🔗", url=f'https://telegram.me/{client.username}?start={base64_string}')]])
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(" 🔗 LINK 🔗 ", url=f'https://telegram.me/{client.username}?start={base64_string}')]])
+    
     try:
         await message.edit_reply_markup(reply_markup)
     except Exception as e:
