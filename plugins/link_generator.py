a6f9aa308ff6d63458319a04080928dcde6faafb
deeganth <118927167+deeganth@users.noreply.github.com>
2022-12-02 23:46:54 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -39,7 +39,7 @@ async def batch(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/{client.username}?start={base64_string}')]])
     
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link} \n\n ``{link}`` ", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link} \n\n {link} ", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
@@ -61,4 +61,4 @@ async def link_generator(client: Client, message: Message):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/{client.username}?start={base64_string}')]])
     
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link} \n\n ``{link}``", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link} \n\n {link}", quote=True, reply_markup=reply_markup)
