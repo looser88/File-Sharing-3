514f6fc63d6613f583a2c15e9b7f624d587051e7
looser88 <98893135+looser88@users.noreply.github.com>
2023-04-26 22:03:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://cheesy:cheesy.8697@cluste
 DB_NAME = os.environ.get("DATABASE_NAME", "fileshare2")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001588924239"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
