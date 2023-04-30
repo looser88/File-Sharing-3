02e6b7feffef60606fceeee8a857d5bde39c1e51
looser88 <98893135+looser88@users.noreply.github.com>
2023-04-30 21:47:18 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -55,13 +55,15 @@ async def start_command(client: Client, message: Message):
                 ids = [int(int(argument[1]) / abs(client.db_channel.id))]
             except:
                 return
-        temp_msg = await message.reply("Please wait...")
+        temp_msg = await message.reply("This video is deleted after 1minute please save or forward the below video")
+        await asyncio.sleep(10)
+        await temp_msg.delete()
         try:
             messages = await get_messages(client, ids)
         except:
             await message.reply_text("Something went wrong..!")
             return
-        await temp_msg.delete()
+    
 
         for msg in messages:
 
