d6273849c3941e897afa5581ac8f254051b61eca
looser88 <98893135+looser88@users.noreply.github.com>
2023-05-10 18:33:07 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -34,7 +34,15 @@ async def channel_post(client: Client, message: Message):
 #   pweek = tomorrow.strftime("%A")
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔗Share URL", url=Tlink)]])
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("↔️Share URL", url=Tlink)]])
-
+try:
+        post_message = await send_message(ECHANNEL_ID,  caption="⚠️Generated for filestore.", sable_notification=False)
+    except FloodWait as e:
+        await asyncio.sleep(e.x)
+        post_message = await send_message(ECHANNEL_ID,  caption="⚠️Generated for filestore.", sable_notification=False)
+    except Exception as e:
+        print(e)
+        await reply_text.edit_text("Something went Wrong..!")
+        else:
     await reply_text.edit(f" <b>▬▬▬▬▬▬▬ ❂ ▬▬▬▬▬▬▬▬</b>\n\n🗓𝐃𝐚𝐭𝐞:- <b>{ptomorrow}</b>\n\n      𝐔𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐛𝐲 :- @Dot_serials_bot \n\n                     ⚜️⚜️⚜️⚜️⚜️⚜️\nᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ :-\n{Slink}\n{Slink}\n\n     👇👇 𝐇𝐨𝐰 𝐭𝐨 𝐨𝐩𝐞𝐧 𝐥𝐢𝐧𝐤👇👇\nhttps://t.me/+Sb5ro1gyhgY0NWM1\nhttps://t.me/+Sb5ro1gyhgY0NWM1", disable_web_page_preview = True)
 
     if not DISABLE_CHANNEL_BUTTON:
@@ -45,15 +53,7 @@ async def new_post(client: Client, message: Message):
 
     if DISABLE_CHANNEL_BUTTON:
         return
-    try:
-        post_message = await send_message(ECHANNEL_ID,  caption="⚠️Generated for filestore.", sable_notification=False)
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        post_message = await send_message(ECHANNEL_ID,  caption="⚠️Generated for filestore.", sable_notification=False)
-    except Exception as e:
-        print(e)
-        await reply_text.edit_text("Something went Wrong..!")
-        return 
+  
     converted_id = message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
