df9b11fb71d3fc690e7d7cb9c39977dda199577f
looser88 <98893135+looser88@users.noreply.github.com>
2023-05-14 00:09:43 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
     
-    await client.send_photo(ECHANNEL_ID, photo="https://graph.org/file/954f49350f9b6e52fa545.jpg" caption="🔥new post detected")
+    await client.send_photo(ECHANNEL_ID, photo="https://graph.org/file/954f49350f9b6e52fa545.jpg", caption="🔥new post detected")
     reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
