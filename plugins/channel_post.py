5d51335c7d7be952a6b08965b845a8733f964c9c
looser88 <98893135+looser88@users.noreply.github.com>
2023-05-17 16:56:29 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -13,14 +13,11 @@ from datetime import datetime, timedelta
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.text & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 def extract_name(client: Client, message: Message):
+    media = message.video or message.document
+    full_str = media.file_name
     spl_word = 'S' 
     res = re.split(spl_word, full_str, maxsplit=1)[0]
     return res
- 
-# Driver Code
-media = message.video or message.document
-full_str = media.file_name
-   # fname = extract_name
 
 def find_pic(extract_name): 
     if extract_name == "Olavina.Nildana.":
