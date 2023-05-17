686d753fea7aab980855d61a3b7a735d9ed703dd
looser88 <98893135+looser88@users.noreply.github.com>
2023-05-17 17:17:03 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -12,7 +12,7 @@ from helper_func import encode
 from datetime import datetime, timedelta
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.text & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
-def extract_name():
+def extract_name(full_str, spl_word):
     media = message.video or message.document
     full_str = media.file_name
     spl_word = 'S' 
