e625012c482889b26ebf1984f8c018ec3e25bf45
looser88 <98893135+looser88@users.noreply.github.com>
2023-05-25 09:47:21 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -29,7 +29,7 @@ def find_pic(extract_name):
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start','users','broadcast','batch','genlink','stats']))
 async def channel_post(client: Client, message: Message):
     
-    e_pic = await client.send_photo(ECHANNEL_ID, photo(find_pic), caption=f"🔥please wait....")
+    e_pic = await client.send_photo(ECHANNEL_ID, photo=f"{find_pic}", caption=f"🔥please wait....")
     reply_text = await message.reply_text("Please Wait...!", quote = True)
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
